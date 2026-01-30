b58fab667b40bfbcde6a424d36b23ba2b721be89
alenazykova2009-cmd <alenazykova2009@mail.ru>
2026-01-30 13:46:33 +0800

    Add files via upload

--- mymoddle.py
+++ mymoddle.py
@@ -1,32 +1,32 @@
-#class Employee:
-#    def __init__(self, name, position):
-#        self.name = name
-#        self.position = position
-#    def display_info(self):
-#        print(f"Сотрудник: {self.name}, Должность: {self.position}")
-#employee = Employee("Нвстя", "Менеджер")
-#employee.display_info()
+class Employee:
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
+    def display_info(self):
+        print(f"Сотрудник: {self.name}, Должность: {self.position}")
+employee = Employee("Нвстя", "Менеджер")
+employee.display_info()
 
 
 
-#class Team:
-#    def __init__(self):
-#        self.team_members = []
-#    def add_member(self, name, position):
-#        member_data = [name, position]
-#        self.team_members.append(member_data)
-#        print(f"Сотрудник {name} добавлен в команду")
-#    def show_team(self):
-#        print("Список команды: ")
-#        if not self.team_members:
-#            print("В команде пока никого нет.")
-#        else:
-#            for member in self.team_members:
-#                print(f"Имя: {member[0]}, Должность: {member[1]}")
-#team = Team()
-#team.add_member("Маша", "Строитель")
-#team.add_member("Юля", "Дизайнер")
-#team.show_team()
+class Team:
+    def __init__(self):
+        self.team_members = []
+    def add_member(self, name, position):
+        member_data = [name, position]
+        self.team_members.append(member_data)
+        print(f"Сотрудник {name} добавлен в команду")
+    def show_team(self):
+        print("Список команды: ")
+        if not self.team_members:
+            print("В команде пока никого нет.")
+        else:
+            for member in self.team_members:
+                print(f"Имя: {member[0]}, Должность: {member[1]}")
+team = Team()
+team.add_member("Маша", "Строитель")
+team.add_member("Юля", "Дизайнер")
+team.show_team()
 
 class Book:
     def __init__(self, name, author, year):
